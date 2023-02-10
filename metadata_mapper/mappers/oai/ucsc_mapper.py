d41b7aaaf1d9859f872d015210c8a3584251cd20
Lucas Thurston <lucas@castironcoding.com>
2023-02-10 09:12:07 -0800

    Reimplement several oai mappers
    Includes: tv_academy, yosemite, up, ucsc

---
+++ metadata_mapper/mappers/oai/ucsc_mapper.py
@@ -0,0 +1,13 @@
+from .oai_mapper import OaiRecord, OaiVernacular
+
+class UcscRecord(OaiRecord):
+
+    def UCLDC_map(self):
+        return {
+            'is_shown_at': self.source_metadata.get('isShownAt'),
+            'is_shown_by': self.source_metadata.get('isShownBy')
+        }
+
+
+class UcscVernacular(OaiVernacular):
+    record_cls = UcscRecord
