4d8677c6253e17ed94122d7a78a23bc0c841551d
Lucas Thurston <lucas@castironcoding.com>
2023-02-16 13:16:58 -0800

    Add csudh and pepperdine mappers

---
+++ metadata_mapper/mappers/oai/contentdm/pepperdine.py
@@ -0,0 +1,17 @@
+from ..contentdm_mapper import ContentdmRecord, ContentdmVernacular
+
+
+class PepperdineRecord(ContentdmRecord):
+    def map_is_shown_by(self):
+        """
+        Does not exclude types containing "sound" like the parent class does
+        """
+        record_type = self.map_type()
+        if not record_type:
+            return
+
+        return self.get_preview_image_url()
+
+
+class PepperdineVernacular(ContentdmVernacular):
+    record_cls = PepperdineRecord
