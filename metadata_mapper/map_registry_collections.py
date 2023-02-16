155607f142c7564bbc168fe3ffb5e987ff85b894
Lucas Thurston <lucas@castironcoding.com>
2023-02-16 13:16:58 -0800

    Add csudh and pepperdine mappers

--- metadata_mapper/map_registry_collections.py
+++ metadata_mapper/map_registry_collections.py
@@ -26,6 +26,8 @@ def map_endpoint(url):
         'black_gold_oai': 'oai.contentdm.blackgold',
         'chico_oai_dc': 'oai.contentdm.chico',
         'chula_vista_pl_contentdm_oai_dc': 'oai.contentdm.cvpl',
+        'contentdm_oai_dc_get_sound_thumbs': 'oai.contentdm.pepperdine',
+        'csudh_contentdm_oai_dc': 'oai.contentdm.csudh'
     }
 
     collection_page = url
